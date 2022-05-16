3175769ca1d7eaf92a47e78742d4f741f8b519ce
alberto006-esp <albertosuarezolvera@gmail.com>
2022-05-16 21:32:01 +0200

    Añadido la ventana de informacion sobre la empresa al mapa de google

--- build/web/js/mapaBusqueda.js
+++ build/web/js/mapaBusqueda.js
@@ -11,73 +11,103 @@
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 function initAutocomplete() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 36.77880260815892, lng: -6.356303784398695 },
-    zoom: 16,
-    mapTypeId: "roadmap",
-  });
-  // Create the search box and link it to the UI element.
-  const input = document.getElementById("pac-input");
-  const searchBox = new google.maps.places.SearchBox(input);
-
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener("bounds_changed", () => {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  let markers = [];
-
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener("places_changed", () => {
-    const places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach((marker) => {
-      marker.setMap(null);
+    const empresa = {lat: 36.77880260815892, lng: -6.356303784398695};
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: empresa,
+        zoom: 18,
+        mapTypeId: "roadmap",
     });
-    markers = [];
+    //Cl. Banda Playa, 65, 11540 Sanlúcar de Barrameda, Cádiz direccion empresa
+    // Create the search box and link it to the UI element.
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
 
-    // For each place, get the icon, name and location.
-    const bounds = new google.maps.LatLngBounds();
-
-    places.forEach((place) => {
-      if (!place.geometry || !place.geometry.location) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-
-      const icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
-
-      // Create a marker for each place.
-      markers.push(
-        new google.maps.Marker({
-          map,
-          icon,
-          title: place.name,
-          position: place.geometry.location,
-        })
-      );
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+        searchBox.setBounds(map.getBounds());
     });
-    map.fitBounds(bounds);
-  });
+    const contentString =
+            '<div id="content">' +
+            '<div id="siteNotice">' +
+            "</div>" +
+            '<h1 id="firstHeading" class="firstHeading">Electro.M</h1>' +
+            '<div id="bodyContent">' +
+            "<p><b>Electro Master S.A.</b>, Fundada en 1980, somos una empresa familiar que empezó como una tienda pequeña de productos electrónicos en Sanlúcar de Barrameda y que con la entrada de las nuevas tecnologías y la venta online nos expandimos a la mayor parte de España.</p>" +
+            '<p>Dirección: Cl. Banda Playa, 65, 11540 Sanlúcar de Barrameda, Cádiz</p>'  +
+            "</div>" +
+            "</div>";
+    const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+    });
+    const icon = "https://developers.google.com/maps/documentation/javascript/examples/full/images/info-i_maps.png";
+    let marker = new google.maps.Marker({
+        position: empresa,
+        icon: icon,
+        draggable: true,
+        map: map,
+        title: "Electro Master S.A.",
+    });
+    marker.addListener("click", () => {
+        infowindow.open({
+            anchor: marker,
+            map: map,
+            shouldFocus: false,
+        });
+    });
+    let markers = [];
+
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+        const places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        /* markers.forEach((marker) => {
+         marker.setMap(null);
+         });*/
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        const bounds = new google.maps.LatLngBounds();
+
+        places.forEach((place) => {
+            if (!place.geometry || !place.geometry.location) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+
+            const icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25),
+            };
+
+            // Create a marker for each place.
+            markers.push(
+                    new google.maps.Marker({
+                        map,
+                        icon,
+                        draggable: true,
+                        title: place.name,
+                        position: place.geometry.location,
+                    })
+                    );
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
 }
 
 window.initAutocomplete = initAutocomplete;
